1d12b4bc9594d52d7db4681b3e2f522e4aafc563
UsernameisKoo <syonkoo@naver.com>
2024-05-29 04:38:10 +0900

    MapMove

--- Assets/Scripts/MapMove.cs
+++ Assets/Scripts/MapMove.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class MapMove : MonoBehaviour
 {
-    public float mapSpeed = 0.1f;
+    public float speed = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(-mapSpeed * Time.deltaTime, 0, 0);
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
 }
