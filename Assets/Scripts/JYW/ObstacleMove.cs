69e86bdfaf0380a215e64863711a1f86f605f03b
UsernameisKoo <syonkoo@naver.com>
2025-02-28 22:44:20 +0900

    ObstacleMove.cs Debugging

--- Assets/Scripts/JYW/ObstacleMove.cs
+++ Assets/Scripts/JYW/ObstacleMove.cs
@@ -1,39 +1,12 @@
 
 using UnityEngine;
-using static SignPostGenerate;
-using static ConeGenerate;
 
 
 public class obstackleMove : MonoBehaviour
 {
     public float speed;
-    public int[] obstacleList = new int [2];
 
-    void obstacleListfunc(int a, int b)
-    {
-        if (a==b)
-        {
-            if (a == 0)
-            {
-                for (int i = 0; i < obstacleList.Length; i++)
-                {
-                    obstacleList[i] = a;
-                }
-            }
-            else if (a == 1)
-            {
-                for (int i = 0; i < obstacleList.Length; i++)
-                {
-                    obstacleList[i] = b;
-                }
-            }
-        }
-        else
-        {
-            obstacleList[a] = a;
-            obstacleList[b] = b;
-        }
-    }
+   
 
     void Awake()
     {
@@ -51,22 +24,7 @@ public class obstackleMove : MonoBehaviour
         {
             transform.Translate(Vector2.right * 55.4f * 3f);
         }
-        if (Time.deltaTime%3==0)
-        {
-            obstacleListfunc(randomValueSP, randomValueSP);
-
-            for (int i = 0;i < obstacleList.Length; i++)
-            {
-                if (obstacleList[i] == randomValueSP)
-                {
-                    GetComponentInChildren(1);
-                }
-                else
-                {
-                    GetComponentInChildren(2);
-                }
-            }
-        }
+        
 
 
     }
